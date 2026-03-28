f9d31ef5af0cf4413aec08a9ae11c1492c0b4abd
Purna Reddy <c.purnachander@gmail.com>
2026-03-28 13:11:19 -0700

    Layout changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 
-REACT_APP_API_URL= "https://smartfurnish-backend.onrender.com"
-
-const API_URL = REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
   const [screen, setScreen] = useState("input");
